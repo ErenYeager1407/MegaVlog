4cf3e3d4f4f03ca290a3072833212e83e77c8c14
Akashdeep Maity <akashdeepmail14@gmail.com>
2025-09-10 14:34:23 +0530

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,43 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from './Button'
+import Input from './input'
+import RTE from "./RTE";
+import Signup from "../pages/Signup";
+import Login from "../pages/Login";
+import PostForm from "./post-form/PostForm";
+import Post from "../pages/Post";
+import AddPost from "../pages/AddPost";
+import PostCard from './PostCard'
+// import PostC
+import AuthLayout from './AuthLayout'
+import Select from './Select'
+import Home from '../pages/Home'
+import AllPosts from "../pages/AllPosts"
+import EditPost from "../pages/EditPost";
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    Input,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    Post,
+    AddPost,
+    PostCard,
+    AuthLayout,
+    Select,
+    Home,
+    AllPosts,
+    EditPost,
+    
+
+}
---
+++ src/conf/conf.js
@@ -0,0 +1,12 @@
+// conf/conf.js
+
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    // Add "APPWRITE_" to all the variable names below
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+}
+
+export default conf;
